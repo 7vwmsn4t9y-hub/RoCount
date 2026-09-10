26b9d68006f8481b4973c8290035f204c6d280a9
romaindarre <7vwmsn4t9y@privaterelay.appleid.com>
2026-09-10 18:47:21 -0300

    Create firebaseconfig.js
    firebase

---
+++ firebaseconfig.js
@@ -0,0 +1,13 @@
+// Configuration Firebase du projet "rocount-53ac0".
+// Ces valeurs ne sont pas des secrets : Firebase les conçoit pour être
+// visibles publiquement dans le code d'une app web. La sécurité réelle
+// vient des règles Firestore (voir LISEZMOI.txt).
+
+window.ECOT_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDxY8zuIbJVfQlrJ9EAZU0vqPK7V97Ai60",
+  authDomain: "rocount-53ac0.firebaseapp.com",
+  projectId: "rocount-53ac0",
+  storageBucket: "rocount-53ac0.firebasestorage.app",
+  messagingSenderId: "1042538355819",
+  appId: "1:1042538355819:web:cbdf48d9338b38fc58652e"
+};
